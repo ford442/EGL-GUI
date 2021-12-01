0e8b41ace92aa69e1e72402a3e2f6842044f6df6
Noah Cohn <noahc42@gmail.com>
2021-12-01 13:05:02 -0800

    Create fs.js

---
+++ fs.js
@@ -0,0 +1,47 @@
+document.getElementById('btn2').addEventListener('click',function(){
+let pth=document.getElementById('path').innerHTML;
+let pth2=document.getElementById('path2').innerHTML;
+let pth3=document.getElementById('path3').innerHTML;
+let pth4=document.getElementById('path4').innerHTML;
+let ff=new XMLHttpRequest();
+let ff2=new XMLHttpRequest();
+let ff3=new XMLHttpRequest();
+let ff4=new XMLHttpRequest();
+ff.open("GET",pth,true);
+ff.responseType="arraybuffer";
+ff.onload=function(oEvent){
+var arrayBuffer=ff.response;
+if(arrayBuffer){
+var fil=new Uint8ClampedArray(arrayBuffer);
+FS.writeFile('/shader/shader1.toy',fil);
+}}
+ff.send(null);
+ff2.open("GET",pth2,true);
+ff2.responseType="arraybuffer";
+ff2.onload=function(oEvent){
+var arrayBuffer2=ff2.response;
+if(arrayBuffer2){
+var fil=new Uint8ClampedArray(arrayBuffer2);
+FS.writeFile('/shader/shader2.toy',fil);
+}}
+ff2.send(null);
+ff3.open("GET",pth3,true);
+ff3.responseType="arraybuffer";
+ff3.onload=function(oEvent){
+var arrayBuffer3=ff3.response;
+if(arrayBuffer3){
+var fil=new Uint8ClampedArray(arrayBuffer3);
+FS.writeFile('/shader/shader3.toy',fil);
+}}
+ff3.send(null);
+ff4.open("GET",pth4,true);
+ff4.responseType="arraybuffer";
+ff4.onload=function(oEvent){
+var arrayBuffer4=ff4.response;
+if(arrayBuffer4){
+var fil=new Uint8ClampedArray(arrayBuffer4);
+FS.writeFile('/shader/shader4.toy',fil);
+console.log('Wrote 4 shaders to filesystem.');
+}}
+ff4.send(null);
+});
